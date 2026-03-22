f69dc02e8f7450999e9fd80dab5487f8ff2e5713
Jhon Kenneth <jhonkenneth@gmail.com>
2026-03-22 19:22:15 +0800

    creation of sign in page

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <form> 
+        <h1>Sign in to your Account</h1>
+        <p>Enter your Email and Password to log in</p>
 
-echo "hbdbcndafdh
+        <label for="email">Email</label><br>
+        <input type="text" name="email" id="email" required>
 
-fix na tong branch na to";
-?>
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" required><br>
+
+        <a>Forgot password?</a><br>
+
+        <button>Log in</button><br>
+
+        <p>or</p>
+
+        <div>
+            <h4>Continue with Facebook</h4>
+            <h4>Continue with Facebook</h4>
+        </div>
+    </form>
+</body>
+</html>
