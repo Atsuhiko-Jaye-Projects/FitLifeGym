3c1ee291154252282f9d69c0c2843c8c49603485
Your Name <you@example.com>
2026-04-02 11:21:27 +0800

    file organized

---
+++ user/client/layout/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wellness Tracker</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <?php 
+    if ($page_title == "work plans") {
+        echo "<link rel='stylesheet' href='../assets/css/workplans.css'>";
+    }
+    ?>
+</head>
+<body>
+
+<div class="container-fluid">
+<div class="row">
---
+++ user/client/layout/sidebar.php
@@ -0,0 +1,33 @@
+<div class="col-md-3 col-lg-2 sidebar p-3">
+
+    <h4 class="mb-4"><i class="bi bi-heart-pulse"></i> FitLife</h4>
+
+    <nav class="nav flex-column">
+
+        <a href="../pages/home.php" class="nav-link">
+            <i class="bi bi-house-door me-2"></i> Home
+        </a>
+
+        <a href="../pages/dashboard.php" class="nav-link">
+            <i class="bi bi-speedometer2 me-2"></i> Dashboard
+        </a>
+
+        <a href="../pages/workout_plans.php" class="nav-link">
+            <i class="bi bi-fire me-2"></i> Workout Plans
+        </a>
+
+        <a href="../pages/progress_tracker.php" class="nav-link">
+            <i class="bi bi-graph-up-arrow me-2"></i> Progress
+        </a>
+
+        <a href="../pages/settings.php" class="nav-link">
+            <i class="bi bi-person-circle me-2"></i> Profile
+        </a>
+
+        <a href="#" class="nav-link">
+            <i class="bi bi-box-arrow-left"></i> Logout
+        </a>
+
+    </nav>
+
+</div>
